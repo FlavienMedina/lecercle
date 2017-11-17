b9aec24aed86d83895a4a7ed99f883aa2190c45b
FlavienMedina <flavien_94@hotmail.fr>
2017-11-17 17:15:49 +0100

    logout

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,17 @@ const db = require('../database/init');
 /* GET home page. */
 router.get('/', (req, res) => {
   if (req.session.users) {
-    res.render('index',{user:req.session.users});
-  }
-  else {
+    res.render('index', {
+      user: req.session.users
+    });
+  } else {
     res.redirect('/signin');
   }
+});
+
+router.get('/logout', (req, res) => {
+  req.session.users = '';
+  res.redirect('/');
 });
 
 module.exports = router;
