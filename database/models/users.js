2af43440e6483f80d12957369a421fbd6ebe50dc
FlavienMedina <flavien_94@hotmail.fr>
2017-11-17 15:49:10 +0100

    check if email and username exist

--- database/models/users.js
+++ database/models/users.js
@@ -1,13 +1,34 @@
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
 
   let users = sequelize.define('user', {
     firstname: DataTypes.STRING,
     lastname: DataTypes.STRING,
     username: DataTypes.STRING,
-    email: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      }
+    },
     birthday: DataTypes.DATE,
     phone: DataTypes.INTEGER,
-    password: DataTypes.STRING
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    password_confirmation: DataTypes.VIRTUAL,
+  }, {
+    hooks: {
+      beforeCreate: user => {
+        if (user.password != user.password_confirmation) {
+          throw "error passwords don't match";
+        }
+        let salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    }
   });
 
   return users;
--- routes/signup.js
+++ routes/signup.js
@@ -8,18 +8,60 @@ router.get('/', (req, res) => {
   res.render('signup');
 });
 router.post('/', (req, res) => {
-  let new_user =
-    db.user.create({
-      firstname: req.body.firstname,
-      lastname: req.body.lastname,
-      username: req.body.username,
-      email: req.body.email,
-      birthday: req.body.birthday,
-      phone: req.body.phone,
-      password: req.body.password,
-    }).then(() => {
-      res.redirect('/');
-    });
+
+  function isFieldUnique(email, username, cb) {
+    return db.user.count({
+        where: {
+          username: username,
+        }
+      })
+      .then(count => {
+        if (count != 0) {
+          console.log(username + ' username already exists');
+          return false;
+        } else {
+          db.user.count({
+              where: {
+                email: email,
+              }
+            })
+            .then(count => {
+              if (count != 0) {
+                console.log(email + ' email already exists');
+                return cb(false);
+              }
+              return cb(true);
+            });
+        }
+
+      });
+  }
+
+  isFieldUnique(req.body.email, req.body.username, (response) => {
+    if (response) {
+      let new_user =
+        db.user.create({
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          username: req.body.username,
+          email: req.body.email,
+          birthday: req.body.birthday,
+          phone: req.body.phone,
+          password: req.body.password,
+        }).then(() => {
+          let user = {
+            firstname: req.body.firstname,
+            lastname: req.body.lastname,
+            username: req.body.username,
+            email: req.body.email
+          }
+          req.session.users = user;
+          res.redirect('/');
+        });
+    }
+  });
+
+
 });
 
 module.exports = router;
